5ffb70e0401190ee96e9f83eed10e4d2f5796629
MariaBertanha <113601174+MariaBertanha@users.noreply.github.com>
2023-09-28 08:56:18 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,15 @@
+   // Pom --------------------------------------
+   function tocaSomPom() {
+   document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelectorAll('.tecla')
+
+
+  
+
+
+
+ 
+ 
+ 
